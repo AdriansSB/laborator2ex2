d58b39d7a24a12a4eda578f8ff30f320b53d7657
AdriansSB <92295503+AdriansSB@users.noreply.github.com>
2021-10-25 14:01:51 +0300

    Add files via upload

--- beta.js
+++ beta.js
@@ -2,19 +2,19 @@ window.addEventListener("deviceorientation", on_device_orientation);
 
 function on_device_orientation(evt)
 {
-	var alpha = evt.alpha;
-	var beta = evt.beta;
-	var gamma = evt.gamma;
+	let alpha = evt.alpha;
+	let beta = evt.beta;
+	let gamma = evt.gamma;
 	
 	document.getElementById("a").innerHTML = "alpha = "+alpha;
 	document.getElementById("b").innerHTML = "beta = "+beta;
 	document.getElementById("c").innerHTML= "gamma = "+gamma;
 	
-	var canvas = document.getElementById("canvas");
-	var context = canvas.getContext("2d");
+	let canvas = document.getElementById("canvas");
+	let context = canvas.getContext("2d");
 	//var centru = {x:canvas.width/2, y:canvas.height/2}
 	//var latura_patrat = 50;
-	var basicRadius = 10;
+	let basicRadius = 10;
 	
 	context.clearRect(0, 0, c.width, c.height);
 	context.fillStyle = "#6A0888";
